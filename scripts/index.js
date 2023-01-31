5266f8c40b510b59a4634ef6d31c58d662e9a6f9
Semen Zavertkin <semenzavertkin@yandex.ru>
2023-01-31 18:58:44 +0700

    второя попытка

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,33 @@
 let popup = document.querySelector('.popup');
 let popupOpen = document.querySelector('.profile__edit-button');
 let btnClose = document.querySelector('.popup__close');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
-let btnSave = document.querySelector('.popup__save');
+let nameInput = document.querySelector('.input_name');
+let jobInput = document.querySelector('.input_job');
 let nameProfile = document.querySelector('.profile__name');
 let jobProfile = document.querySelector('.profile__job');
 let formElement = popup.querySelector('.popup__form')
 
 
-let popupClose = () => {
-    popup.classList.remove('popup__opened');
+let сlosePopup = () => {
+    popup.classList.remove('popup_opened');
 }
 
 popupOpen.addEventListener('click', ()=> {
-    popup.classList.add('popup__opened')
+    popup.classList.add('popup_opened')
     nameInput.value = nameProfile.textContent;
     jobInput.value = jobProfile.textContent;
 })
 
-btnClose.addEventListener('click', ()=> {
-    popupClose();
-})
+
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
     nameProfile.textContent = nameInput.value;
     jobProfile.textContent = jobInput.value;
-    popupClose();
+    сlosePopup();
 }
 
-formElement.addEventListener('submit', handleFormSubmit); 
+formElement.addEventListener('submit', handleFormSubmit);
+btnClose.addEventListener('click', ()=> {
+    сlosePopup();
+})
